ce1e5d2d6e4489e06d3f2fd6b1947b3ac24a16b7
gnessdo <87198355+gnessdo@users.noreply.github.com>
2021-10-23 18:43:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let inp = document.querySelector("#inp").value;
+let btn = document.querySelector("#btn");
+let text = document.querySelector("basliq");
+// let bosluq = document.querySelector("#show");
+
+
+
+
+btn.addEventListener("click", function Change(){    
+    document.querySelector("#show").innerHTML = inp;
+
+
+}
+
+
+// x.addEventListener("click" ,Test )
+
+// function Test(e){
+//     alert("test")
+// }
+
+
+// let x = document.querySelector(".container");
+
+// x.addEventListener("mouseenter" , Test )
+// x.addEventListener("mouseleave" , Test )
+// x.addEventListener("mouseout" , Test );
+// x.addEventListener("mouseover" , Test )
+// x.addEventListener("mousemove" , Test )
+
+
+// function Test(e){
+//    let show = document.querySelector("#show");
+
+//    show.innerHTML =  ` X : ${e.clientX} - Y : ${e.clientY} `
+
+//    x.style.backgroundColor = `rgb( ${e.clientX}, ${e.clientY}, ${e.clientY+ e.clientX}) `
+
+// }
+
+
+
+
